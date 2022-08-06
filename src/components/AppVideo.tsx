eea01cb7544702a2a65fd5323f47803aa607b5bc
Moraru <tiltiobusiness@gmail.com>
2022-08-07 00:34:12 +0300

    fix

--- src/components/AppVideo.tsx
+++ src/components/AppVideo.tsx
@@ -2,7 +2,7 @@ import videoSource from "../assets/icons/vpreview.mp4";
 
 const AppVideo = () => {
   return (
-    <video controls={false} autoPlay loop>
+    <video controls={false} autoPlay loop playsInline muted>
       <source src={videoSource} type="video/mp4" />
     </video>
   );
